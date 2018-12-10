a88dd8289e28fc564cec6d34bf3ae14175355637
Dgiovan <32046529+Dgiovan@users.noreply.github.com>
2018-12-09 23:11:48 -0600

    gio
    Profiles she/he
    metod databases

--- app/src/main/java/com/serie/youtube/dagio/androidparatodos/Activitys/MenuActivity.java
+++ app/src/main/java/com/serie/youtube/dagio/androidparatodos/Activitys/MenuActivity.java
@@ -1,8 +1,16 @@
 package com.serie.youtube.dagio.androidparatodos.Activitys;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -10,26 +18,56 @@ import android.widget.Toast;
 import com.nightonke.boommenu.BoomButtons.OnBMClickListener;
 import com.nightonke.boommenu.BoomButtons.SimpleCircleButton;
 import com.nightonke.boommenu.BoomMenuButton;
+import com.serie.youtube.dagio.androidparatodos.ConexionSQLiteHelper;
+import com.serie.youtube.dagio.androidparatodos.Fragments.ProfileHe;
 import com.serie.youtube.dagio.androidparatodos.R;
+import com.serie.youtube.dagio.androidparatodos.Utilidades.Utilidades;
 
 import java.util.ArrayList;
 
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity  implements View.OnClickListener {
 
+    //Elementos graficos
+    TextView name;
+    LinearLayout perfil;
 
-private BoomMenuButton bmb;
-ArrayList<Integer> imageIDList;
+    //Boon Menu
+    private BoomMenuButton bmb;
+    ArrayList<Integer> imageIDList;
+
+    //Conexion base de datos
+    ConexionSQLiteHelper conn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         bmb=findViewById(R.id.bmb);
+        name =findViewById(R.id.nameprofile);
+        perfil=findViewById(R.id.Lprofile);
+
+
         imageIDList=new ArrayList<>();
         setInitialData();
 
+        conn=new ConexionSQLiteHelper(getApplicationContext(),"bd_usuarios",null,1);
+        SQLiteDatabase db=conn.getReadableDatabase();
+        String [] nombreusuario={Utilidades.CAMPO_NOMBRE};
+        try {
 
+            Cursor cursor =db.query(Utilidades.TABLA_USUARIO,nombreusuario, String.valueOf(1),null,null,null,null);
+            cursor.moveToFirst();
+            name.setText(cursor.getString(0));
+            cursor.close();
+
+        }catch (Exception e){
+            Log.e("TAG","Algo esta mal con la basede datos");
+        }
+
+
+
+        //metodo para boom menu
 
         for (int i = 0; i < bmb.getPiecePlaceEnum().pieceNumber(); i++) {
             SimpleCircleButton.Builder builder = new SimpleCircleButton.Builder()
@@ -61,5 +99,18 @@ ArrayList<Integer> imageIDList;
         imageIDList.add(R.drawable.xml);
         imageIDList.add(R.drawable.java);
         imageIDList.add(R.drawable.gradlelogo);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        Intent intent=null;
+        switch (view.getId()){
+            case R.id.Lprofile :
+                intent= new Intent(MenuActivity.this,ProfileHe.class);
+                startActivity(intent);
+
+                break;
+        }
     }
 }
