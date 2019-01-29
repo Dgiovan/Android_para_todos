786b5fd6358a113e83d540b56fd2e69d742fbac4
Dgiovan <32046529+Dgiovan@users.noreply.github.com>
2019-01-28 22:14:26 -0600

    gio
    add content

--- app/src/main/java/com/serie/youtube/dagio/androidparatodos/Fragments/Contents/appOne.java
+++ app/src/main/java/com/serie/youtube/dagio/androidparatodos/Fragments/Contents/appOne.java
@@ -1,5 +1,6 @@
 package com.serie.youtube.dagio.androidparatodos.Fragments.Contents;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -7,18 +8,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.serie.youtube.dagio.androidparatodos.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link appOne.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link appOne#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class appOne extends Fragment {
+
+public class appOne extends Fragment  {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -27,6 +25,7 @@ public class appOne extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    ImageView login;
 
     private OnFragmentInteractionListener mListener;
 
@@ -34,14 +33,7 @@ public class appOne extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment appOne.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static appOne newInstance(String param1, String param2) {
         appOne fragment = new appOne();
@@ -64,8 +56,38 @@ public class appOne extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_app_one, container, false);
+        View v=inflater.inflate(R.layout.fragment_app_one, container, false);
+        login= v.findViewById(R.id.Imlogin);
+
+       login.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               final Dialog LOGIN= new Dialog(getContext());
+               final LinearLayout mylogin;
+               LOGIN.setContentView(R.layout.optionlogin);
+               LOGIN.show();
+               mylogin=LOGIN.findViewById(R.id.LayoutLogin);
+               mylogin.setOnClickListener(new View.OnClickListener() {
+                   @Override
+                   public void onClick(View v) {
+                       final Button text;
+                       text=LOGIN.findViewById(R.id.mytest);
+                       text.setOnClickListener(this);
+
+                       switch (v.getId())
+                       {
+                           case R.id.mytest:
+                               Toast.makeText(getContext(), "Exito", Toast.LENGTH_SHORT).show();
+                               break;
+                       }
+
+                   }
+               });
+           }
+       });
+
+
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -92,16 +114,7 @@ public class appOne extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
